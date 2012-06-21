36a0fd687aa022253df923e33c692d31eaedcdb9
yurak <yurak.sisa.dream@gmail.com>
2012-06-21 11:29:48 +0200

    encoding

--- lib/sinatra/ysd-plugins_hookcall.rb
+++ lib/sinatra/ysd-plugins_hookcall.rb
@@ -14,22 +14,8 @@ module Plugins
       #   The context
       #
       def call_hook( hook, context={})
-         
-         #return_value = ""
-              
-         #Plugins::Plugin.plugins.each do |plugin_name, plugin|
-         #  
-         #  plugin.hooker_instances.each do |hooker_instance|
-         #    if hooker_instance.class.method_defined?(hook)
-         #      return_value << hooker_instance.send(hook, context.merge(:app => self))
-         #    end
-         #  end
-         #  
-         #end
-         #         
-         #return_value
-       
-         Plugins::Plugin.plugin_invoke_all(hook, context.merge(:app => self)).join 
+                
+         Plugins::Plugin.plugin_invoke_all(hook, context.merge(:app => self)).join.force_encoding('utf-8') 
        
       end
     
